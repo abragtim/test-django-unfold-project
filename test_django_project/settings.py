d3ab0d061b060d8ed8d93cc0d3adae277cc8085f
abragtim <timur.abragimovich@mild.blue>
2025-04-10 19:11:10 +0200

    Add static root.

--- test_django_project/settings.py
+++ test_django_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Directory where the static files will be collected to
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Path to collect static files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
